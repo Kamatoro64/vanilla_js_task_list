81d18f1b8a701ba93f527a007bd8ab8c02f86fc3
NG Chester <zhentingchester.ng@murex.com>
2019-07-20 23:27:13 +1000

    Add event listener to form element on sumbit and execute callback addTask. Note that addTask takes an event object as its argument. A check is done to see if the inout is empty and e.preventDefault prevents the default action of form submission

--- app.js
+++ app.js
@@ -3,4 +3,21 @@ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task')
+const taskInput = document.querySelector('#task');
+
+//Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+	// Add task event
+	form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e) {
+	if (taskInput.value === '') {
+		alert('Add a task');
+	}
+
+	e.preventDefault()
+}
