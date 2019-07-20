8896a9357a07379ac02911d8f999f5ba7daa636f
NG Chester <zhentingchester.ng@murex.com>
2019-07-20 23:49:24 +1000

    Finishing up addTask function to create our li element. Notice that apart from adding a text node to the li element, we're also adding a link in which we're adding a font awesome delete icon

--- app.js
+++ app.js
@@ -19,5 +19,41 @@ function addTask(e) {
 		alert('Add a task');
 	}
 
+	// Create li element
+	const li = document.createElement('li');
+
+	// In materialize for lists to look good the ul should have class collection and
+	// li should have class collection-item
+	li.className = 'collection-item';
+
+	// Interesting, why can't we just set the li's text directly?
+	li.appendChild(document.createTextNode(taskInput.value));
+
+	// Create new link element
+	const link = document.createElement('a');
+	link.className = 'delete-item secondary-content'; //Look up secondary-content in materialize
+
+	link.innerHTML = '<i class="fa fa-remove"></i>';
+
+	//Append the link to li
+
+	li.appendChild(link);
+
+	// Append li to ul
+
+	taskList.appendChild(li);
+
+	// Clear input
+
+	taskInput.value = '';
+	//console.log(li);
+
+
+
+
+
+
+
+
 	e.preventDefault()
 }
